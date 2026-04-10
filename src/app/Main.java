9c0a2fbf36980399292b4a232932e4800f7423f5
Roman-orn <0671635566@ukr.net>
2026-04-11 00:24:36 +0300

    Add convert Frt to Cls

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     static void main() {
         System.out.println("App converter temperature");
+
+        double frt = 50;
+        double resCls = fahrenheitToCelsius(frt);
+        System.out.printf("%.2f degrees Fahrenheit equals %.2f degrees Celsius;\n", frt, resCls);
+    }
+
+    static double fahrenheitToCelsius(double frt){
+        return (frt - 32) / 1.8;
     }
 }
