8f8113c071bf751b28782c61d604d9e8ae4e19a4
Roman-orn <0671635566@ukr.net>
2026-04-14 15:54:14 +0300

    Convert temperature (#2)
    * Add convert Frt to Cls
    
    * Add convert Cls to Frt
    
    * Move magic numbers to constants (#3)
    
    * Move magic numbers to constants
    
    * Rename constants

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,25 @@
 package app;
 
 public class Main {
+    static final double OFFSET = 32;
+    static final double SCALING_FACTOR = 1.8;
 
     static void main() {
         System.out.println("App converter temperature");
+
+        double frt = 50;
+        double cls = 20;
+        double resCls = fahrenheitToCelsius(frt);
+        double resFrt = celsiusToFahrenheit(cls);
+        System.out.printf("%.2f degrees Fahrenheit equals %.2f degrees Celsius;\n", frt, resCls);
+        System.out.printf("%.2f degrees Celsius equals %.2f degrees Fahrenheit;\n", cls, resFrt);
+    }
+
+    static double fahrenheitToCelsius(double frt){
+        return (frt - OFFSET) / SCALING_FACTOR;
+    }
+
+    static double celsiusToFahrenheit(double cls){
+        return cls * SCALING_FACTOR + OFFSET;
     }
 }
